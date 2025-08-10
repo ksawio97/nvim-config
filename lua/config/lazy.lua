929fcb37cc2c9650fb182fe10f75fa5521344b35
ksawio <ksawery9708@gmail.com>
2025-08-10 20:43:33 +0200

    added incremental selection and textobjects for tree-sitter

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,20 +29,51 @@ require("lazy").setup({
             vim.cmd.colorscheme("kanagawa-wave")
         end
     },
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ":TSUpdate",
-        lazy = false,
-        branch = "main",
-	setup = function ()
-		require("nvim-treesitter.config").setup {
-			auto_install = true,
 
-			highlight = { 
-				enable = true,
-			},
-		}
-	end,
+    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "master",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup {
+                highlight = { enable = true },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<leader>ss",
+                        node_incremental = "<leader>si",
+                        node_decremental = "<leader>sd",
+                        scope_incremental = "<leader>sc",
+                    },
+                },
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = "@class.inner",
+                        },
+                        selection_modes = {
+                            ['@parameter.outer'] = 'v', -- charwise
+                            ['@function.outer'] = 'V', -- linewise
+                            ['@class.outer'] = '<c-v>', -- blockwise
+                        },
+                        include_surrounding_whitespace = true,
+                    },
+                },
+            }
+        end,
     },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        branch = "master",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            require("nvim-treesitter-textobjects")
+        end,
+    }
 })
 
