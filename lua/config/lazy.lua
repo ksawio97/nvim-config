c98f5a2d495df200390e28db21059bc40d234d54
ksawio <ksawery9708@gmail.com>
2025-08-15 14:57:50 +0200

    added lualine

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -92,5 +92,13 @@ require("lazy").setup({
             "neovim/nvim-lspconfig",
         },
     },
+    {
+        -- nerd font is required
+        "nvim-lualine/lualine.nvim",
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function ()
+            require('lualine').setup()
+        end
+    },
 })
 
